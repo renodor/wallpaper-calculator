bc0004dd5c1d3887f5f0a6394c3308562a479098
renaud dor <renaud.dor@gmail.com>
2020-08-26 18:13:42 +0200

    remove bootstrap and create a custom modal

---
+++ assets/js/modal.js
@@ -0,0 +1,26 @@
+const modalBtn = document.getElementById('wallpaper-calculator-modal-btn');
+const modal = document.querySelector('.renodor-modal');
+const closeModalBtns = document.querySelectorAll('.renodor-modal .close-modal');
+
+closeModal = () => {
+  modal.style.display = 'none';
+};
+
+modalBtn.addEventListener('click', (event) => {
+  modal.style.display = 'flex';
+
+  modal.addEventListener('click', (event) => {
+    closeModal();
+  });
+
+  modal.querySelector('.modal-container').addEventListener('click', (event) => {
+    event.stopPropagation();
+  });
+});
+
+closeModalBtns.forEach((closeModalBtn) => {
+  closeModalBtn.addEventListener('click', (event) => {
+    closeModal()
+  });
+});
+
